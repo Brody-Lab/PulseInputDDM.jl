15c66286374bbd6682f3686ce34d0d866536477f
Brian DePasquale <depasquale@princeton.edu>
2019-01-23 22:06:54 -0500

    yo
    Former-commit-id: ffacf8a3e00d7a6a14ea02b268881cbcd4e658b6 [formerly d1226c9b0899bba0f59ec0be83ee310151fba6e7]
    Former-commit-id: ad88b67a86ff93cac1427ed6fe69690390a1357a
    Former-commit-id: d5a23299cdf00619a308dd3a8068fbc91e9a2b9b [formerly b71dba6d023ba8a2e4027ec35bac8910a94637c3]
    Former-commit-id: aa5c97c2dd2312f720fd7ff6d8cbb26a2f3fe96b
    Former-commit-id: 1fedbacada36a565b95d32474b4f845f07ef2300
    Former-commit-id: c2fea043781904e5e6eea41237b876a44159eedf
    Former-commit-id: a8fdf6222992b583cab6819aa1116b151b71f46e

--- src/spike_data_latent_accum.jl
+++ src/spike_data_latent_accum.jl
@@ -1,5 +1,10 @@
 module spike_data_latent_accum
 
-greet() = print("Hello World!")
+using choice_observation, choice_and_poisson_neural_observation
+using gaussian_neural_observation, helpers, initalize_latent_model
+using initialize_spike_obs_model, latent_DDM_common_funcdtions
+using poisson_neural_observation
+
+export choice_observation
 
 end # module
