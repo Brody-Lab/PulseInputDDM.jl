0f2c868009874d3e2c325ebc6e15eee00ec1c224
autodocs <autodocs>
2019-01-26 18:37:38 +0000

    build based on b46140c

---
+++ dev/search_index.js
@@ -0,0 +1,107 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "#",
+    "page": "Home",
+    "title": "Home",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "#Latent-DDM-Documentation-1",
+    "page": "Home",
+    "title": "Latent DDM  Documentation",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "#Functions-1",
+    "page": "Home",
+    "title": "Functions",
+    "category": "section",
+    "text": "poiss_LL(k,λ,dt)"
+},
+
+{
+    "location": "#Index-1",
+    "page": "Home",
+    "title": "Index",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "tutorial/page1/#",
+    "page": "-",
+    "title": "-",
+    "category": "page",
+    "text": "#TitleSome text describing this section"
+},
+
+{
+    "location": "tutorial/page1/#spike_data_latent_accum.poiss_LL-Tuple{Any,Any,Any}",
+    "page": "-",
+    "title": "spike_data_latent_accum.poiss_LL",
+    "category": "method",
+    "text": "poiss_LL(k,λ,dt)  \n\nreturns poiss LL\n\n\n\n\n\n"
+},
+
+{
+    "location": "tutorial/page1/#Subtitle-1",
+    "page": "-",
+    "title": "Subtitle",
+    "category": "section",
+    "text": "poiss_LL(k,λ,dt)"
+},
+
+{
+    "location": "tutorial/page2/#",
+    "page": "-",
+    "title": "-",
+    "category": "page",
+    "text": "#TitleSome text describing this section"
+},
+
+{
+    "location": "tutorial/page2/#Subtitle-1",
+    "page": "-",
+    "title": "Subtitle",
+    "category": "section",
+    "text": "poiss_LL(k,λ,dt)"
+},
+
+{
+    "location": "sec2/page1/#",
+    "page": "-",
+    "title": "-",
+    "category": "page",
+    "text": "#TitleSome text describing this section"
+},
+
+{
+    "location": "sec2/page1/#Subtitle-1",
+    "page": "-",
+    "title": "Subtitle",
+    "category": "section",
+    "text": "poiss_LL(k,λ,dt)"
+},
+
+{
+    "location": "sec2/page2/#",
+    "page": "-",
+    "title": "-",
+    "category": "page",
+    "text": "#TitleSome text describing this section"
+},
+
+{
+    "location": "sec2/page2/#Subtitle-1",
+    "page": "-",
+    "title": "Subtitle",
+    "category": "section",
+    "text": "poiss_LL(k,λ,dt)"
+},
+
+]}
