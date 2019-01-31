ae77d021011e92c1d31b1b9f024b57d61aeccb03
Brian DePasquale <depasquale@princeton.edu>
2019-01-30 20:48:58 -0500

    added Random
    Former-commit-id: 0f3c04ef2383acb4abb61c4d83decf139704bf1e [formerly da4954dc1740719715feec17640d4f3d3e10bd29]
    Former-commit-id: aec4b1c91f52db00f925f288ab264adcbe95591a
    Former-commit-id: 79f578026775285356547034be1460163951106b [formerly 582aca5878b252abb6c92805480b8532de4bc2b7]
    Former-commit-id: a5c54dfc5bf0eb87558817d5219d6cb117cff864
    Former-commit-id: 35fb94e730fc521f21bef7b848aa3c8a7715e162
    Former-commit-id: c3d54c5a92a7a3e9f14a26a7dd3653cac7ab0175
    Former-commit-id: e62ccfed65f23b82c126425ea34d9672926b1802

--- src/run_funcs.jl
+++ src/run_funcs.jl
@@ -222,7 +222,8 @@ function do_optim_ΔLR(dt::Float64,data::Dict,fit_vec::Union{Vector{BitArray{1}}
         mu0::Vector{Vector{Float64}}=Vector{Vector{Float64}}(),
         f_str::String="softplus",
         x_tol::Float64=1e-16,f_tol::Float64=1e-16,g_tol::Float64=1e-12,
-        iterations::Int=Int(5e3),show_trace::Bool=false)
+        iterations::Int=Int(5e3),show_trace::Bool=false,
+        muf::Vector{Vector{Float64}}=Vector{Vector{Float64}}())
     
         ###########################################################################################
         ## Compute p0 by linear regression
@@ -237,8 +238,8 @@ function do_optim_ΔLR(dt::Float64,data::Dict,fit_vec::Union{Vector{BitArray{1}}
         ## Map parameters to unbounded domain for optimization
         inv_map_py!.(p,f_str=f_str)
     
-        p = pmap((p,trials,k,fit_vec)->do_optim_ΔLR_single(p,dt,ΔLR[trials],k,fit_vec;
-            show_trace=show_trace,f_str=f_str),p,trials,SC,fit_vec)
+        p = pmap((p,trials,k,fit_vec,muf)->do_optim_ΔLR_single(p,dt,ΔLR[trials],k,fit_vec;
+            show_trace=show_trace,f_str=f_str,muf=muf),p,trials,SC,fit_vec,muf)
     
         ###########################################################################################
         ## Remap to bounded domain
@@ -251,13 +252,14 @@ function do_optim_ΔLR_single(p::Vector{Float64},dt::Float64,ΔLR::Vector{Vector
         beta::Vector{Float64}=Vector{Float64}(),
         mu0::Vector{Float64}=Vector{Float64}(),f_str::String="softplus",
         x_tol::Float64=1e-16,f_tol::Float64=1e-16,g_tol::Float64=1e-12,iterations::Int=Int(5e3),
-        show_trace::Bool=false)
+        show_trace::Bool=false,
+        muf::Vector{Float64}=Vector{Float64}())
         
         p_opt,p_const = inv_gather(p,fit_vec)
     
         ###########################################################################################
         ## Optimize    
-        ll(p_opt) = ll_wrapper_ΔLR(p_opt, p_const, fit_vec, k, ΔLR, dt; beta=beta, mu0=mu0, f_str=f_str)
+        ll(p_opt) = ll_wrapper_ΔLR(p_opt, p_const, fit_vec, k, ΔLR, dt; beta=beta, mu0=mu0, f_str=f_str, muf=muf)
         opt_output, state = opt_ll(p_opt,ll;g_tol=g_tol,x_tol=x_tol,f_tol=f_tol,iterations=iterations,show_trace=show_trace)
         p_opt = Optim.minimizer(opt_output)
     
@@ -271,16 +273,19 @@ function ll_wrapper_ΔLR(p_opt::Vector{TT}, p_const::Vector{Float64},fit_vec::Un
         k::Vector{Vector{Int}}, ΔLR::Vector{Vector{Int}}, dt::Float64;
         beta::Vector{Float64}=Vector{Float64}(0),
         mu0::Vector{Float64}=Vector{Float64}(0),
-        f_str::String="softplus") where {TT}
+        f_str::String="softplus",
+        muf::Vector{Float64}=Vector{Float64}()) where {TT}
     
         p = gather(p_opt, p_const, fit_vec)
        
         #check fy because of NaN poiss_LL fiasco
         map_py!(p,f_str=f_str)
         λ = fy.([p],vcat(ΔLR...),f_str=f_str)
+        λ0 = vcat(map(x->muf[1:length(x)],ΔLR)...)
     
         #-(sum(poiss_LL(λ,vcat(k...),dt)) - sum(gauss_prior(py,mu0,beta)))
-        LL = sum(poiss_LL.(vcat(k...),λ,dt))
+        #LL = sum(poiss_LL.(vcat(k...),λ,dt))
+        LL = sum(poiss_LL.(vcat(k...),λ+λ0,dt))
         length(beta) > 0 ? LL += sum(gauss_prior.(p,mu0,beta)) : nothing
     
         return -LL
