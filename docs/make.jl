08eb51769087a1882ab002ba729ccee4ca122275
Brian DePasquale <depasquale@princeton.edu>
2019-02-08 00:15:24 -0500

    new docs
    Former-commit-id: 32e58439f9a09cf7c0084073f5532e83fc896b1b [formerly 9a6126220b8850c64587febc2e9f05540f0eb859]
    Former-commit-id: f2b613f89c7cf351bbf43b26614b05dc6d60be85
    Former-commit-id: 48347b6850df6205efb2eecc95e687edc112c606 [formerly b229a947e4fa19ff528d54ee2975449252c9ae4e]
    Former-commit-id: 237fd85be1de35a555cbbcb5c1583fb5378620f6
    Former-commit-id: 77335c2437b7e46d1499219120c38f7973adf52d
    Former-commit-id: 1ca1117072419825c10bed329be625ba388230cc
    Former-commit-id: 565f4074d9c58e422ad6d04d784a0c677a66903a

--- docs/make.jl
+++ docs/make.jl
@@ -2,7 +2,7 @@ push!(LOAD_PATH,"../src/")
 
 using Documenter, pulse_input_DDM
 
-makedocs(sitename="pulse_input_DDM", modules=[pulse_input_DDM], doctest=false,
+makedocs(sitename="pulse input DDM", modules=[pulse_input_DDM], doctest=false,
         pages = Any[
         "Home" => "index.md",
         "Getting Started" => Any[
