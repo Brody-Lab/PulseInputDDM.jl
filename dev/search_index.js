dfc5aaa600f8d1c841e27e9695a912fdea44a2e2
autodocs <autodocs>
2019-02-08 06:13:21 +0000

    build based on 91cb34a

--- dev/search_index.js
+++ dev/search_index.js
@@ -9,9 +9,9 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "#Pulse*input*DDM-Documentation-1",
+    "location": "#pulse-input-DDM-Documentation-1",
     "page": "Home",
-    "title": "PulseinputDDM  Documentation",
+    "title": "pulse input DDM  Documentation",
     "category": "section",
     "text": "Code for fitting latent drift diffusion models to pulsed input data and neural activity or behavioral observation data.Pages = [\n    \"man/choice_observation_model.md\",\n    \"man/neural_observation_model.md\"]\nDepth = 2"
 },
@@ -21,7 +21,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "Functions",
     "category": "section",
-    "text": "poiss_LL(k,λ,dt)"
+    "text": "    optimize_model(pz, bias, pz_fit_vec, bias_fit_vec,\n        data; dt, n, map_str, x_tol,f_tol,g_tol, iterations)"
 },
 
 {
@@ -53,7 +53,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Fitting a model to choice observations",
     "title": "Some important functions",
     "category": "section",
-    "text": "poiss_LL(k,λ,dt)"
+    "text": "    optimize_model(pz, bias, pz_fit_vec, bias_fit_vec,\n        data; dt, n, map_str, x_tol,f_tol,g_tol, iterations)"
 },
 
 {
@@ -73,19 +73,11 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "man/neural_observation_model/#pulse_input_DDM.poiss_LL-Tuple{Any,Any,Any}",
-    "page": "Fitting a model to neural activity",
-    "title": "pulse_input_DDM.poiss_LL",
-    "category": "method",
-    "text": "poiss_LL(k,λ,dt)  \n\nreturns poiss LL\n\n\n\n\n\n"
-},
-
-{
     "location": "man/neural_observation_model/#Some-important-functions-1",
     "page": "Fitting a model to neural activity",
     "title": "Some important functions",
     "category": "section",
-    "text": "poiss_LL(k,λ,dt)"
+    "text": "    optimize_model(pz,py,pz_fit,py_fit,data;\n        dt::Float64=1e-2, n::Int=53, f_str=\"softplus\",map_str::String=\"exp\",\n        beta::Vector{Vector{Float64}}=Vector{Vector{Float64}}(),\n        mu0::Vector{Vector{Float64}}=Vector{Vector{Float64}}(),\n        x_tol::Float64=1e-16,f_tol::Float64=1e-16,g_tol::Float64=1e-12,\n        iterations::Int=Int(5e3),show_trace::Bool=true, \n        λ0::Vector{Vector{Float64}}=Vector{Vector{Float64}}())"
 },
 
 ]}
