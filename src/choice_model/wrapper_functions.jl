d0cad0f4de28fa15cb7fe2fe6f08945d1fc55354
briandepasquale <depasquale@princeton.edu>
2019-09-25 21:16:50 -0400

    blah
    Former-commit-id: e9d5ce277f7df7af12b9638e4dadeb04f974a927 [formerly 9a2cc91fdb78605679f30acc773253aee9a90f00]
    Former-commit-id: 22eed91a0223f524c0700e19f2a2e35002aba2cb
    Former-commit-id: 712d2d3c03167fa57476076bfcb87fcfae065fd1 [formerly dfb007a7a5c30398f8532a192d039e897aa94d45]
    Former-commit-id: 740db537e3b4ad7e8e3a28cfc39a5bd23cd9c523
    Former-commit-id: c29048d5f6b2b5dfee1f2b979d39888bb00492ea
    Former-commit-id: 0be9330849a51376f418bbcce7f8ee19f3350e61
    Former-commit-id: 9f85df7c6a2a1dbe7744cc8b0f33e3bb4fc1ebf4

--- src/choice_model/wrapper_functions.jl
+++ src/choice_model/wrapper_functions.jl
@@ -1,65 +1,65 @@
 #################################### Choice observation model #################################
 
-function generate_syn_data_fit_CI(pz::Dict{}, pd::Dict{}, ntrials::Int; 
+function generate_syn_data_fit_CI(pz::Dict{}, pd::Dict{}, ntrials::Int;
         n::Int=53, dt=1e-2, use_bin_center::Bool=false)
-   
+
     data = sample_inputs_and_choices(pz["generative"], pd["generative"], ntrials;
         use_bin_center=use_bin_center)
-    
+
     data = bin_clicks!(data,use_bin_center;dt=dt)
-    
-    pz, pd, = optimize_model(pz, pd, data; n=n, show_trace=true)   
+
+    pz, pd, = optimize_model(pz, pd, data; n=n, show_trace=true)
     pz, pd = compute_H_CI!(pz, pd, data; n=n)
 
 end
 
 function compute_Hessian(pz, pd, data; n::Int=53) where {TT <: Any}
-    
+
     fit_vec = combine_latent_and_observation(pz["fit"], pd["fit"])
     p_opt, p_const = split_combine_invmap(pz["final"], pd["final"], fit_vec, data["dt"], pz["lb"], pz["ub"])
-    
+
     parameter_map_f(x) = map_split_combine(x, p_const, fit_vec, data["dt"], pz["lb"], pz["ub"])
     ll(x) = ll_wrapper(x, data, parameter_map_f, n=n)
 
     return H = ForwardDiff.hessian(ll, p_opt);
-        
+
 end
 
 
 function compute_H_CI!(pz, pd, data; n::Int=53)
-    
+
     H = compute_Hessian(pz, pd, data; n=n)
-    
+
     gooddims = 1:size(H,1)
-    
+
     evs = findall(eigvals(H[gooddims,gooddims]) .<= 0)
     otherbad = vcat(map(i-> findall(abs.(eigvecs(H[gooddims,gooddims])[:,evs[i]]) .> 0.5), 1:length(evs))...)
     gooddims = setdiff(gooddims,otherbad)
 
     fit_vec = combine_latent_and_observation(pz["fit"], pd["fit"])
-    p_opt, p_const = split_combine_invmap(copy(pz["final"]), copy(pd["final"]), 
+    p_opt, p_const = split_combine_invmap(copy(pz["final"]), copy(pd["final"]),
         fit_vec, data["dt"], pz["lb"], pz["ub"])
 
     CI = fill!(Vector{Float64}(undef,size(H,1)),1e8);
 
     CI[gooddims] = 2*sqrt.(diag(inv(H[gooddims,gooddims])))
-    
+
     parameter_map_f(x) = map_split_combine(x, p_const, fit_vec, data["dt"], pz["lb"], pz["ub"])
 
     pz["CI_plus"], pd["CI_plus"] = parameter_map_f(p_opt + CI)
     pz["CI_minus"], pd["CI_minus"] = parameter_map_f(p_opt - CI)
-    
-    #identify which ML parameters have generative parameters within the CI 
+
+    #identify which ML parameters have generative parameters within the CI
     if haskey(pz, "generative")
         pz["within_CI"] = (pz["CI_minus"] .< pz["generative"]) .& (pz["CI_plus"] .> pz["generative"])
     end
-    
+
     if haskey(pd, "generative")
         pd["within_CI"] = (pd["CI_minus"] .< pd["generative"]) .& (pd["CI_plus"] .> pd["generative"])
     end
-    
+
     return pz, pd, H
-    
+
 end
 
 function load_and_optimize(path::String, sessids, ratnames; n::Int=53, dt::Float64=1e-2,
@@ -70,15 +70,15 @@ function load_and_optimize(path::String, sessids, ratnames; n::Int=53, dt::Float
         "ub" => [10., 100, 5., 800., 40., 2., 10.]),
         show_trace::Bool=true, iterations::Int=Int(2e3),
         x_tol::Float64=1e-4, f_tol::Float64=1e-9, g_tol::Float64=1e-2)
-    
+
     data = aggregate_choice_data(path,sessids,ratnames)
     data = bin_clicks!(data,use_bin_center;dt=dt)
-        
+
     pz, pd = load_and_optimize(data; n=n,
         pz=pz, show_trace=show_trace, iterations=iterations)
-    
+
     return pz, pd
-    
+
 end
 
 function load_and_optimize(data; n::Int=53,
@@ -91,13 +91,13 @@ function load_and_optimize(data; n::Int=53,
         x_tol::Float64=1e-4, f_tol::Float64=1e-9, g_tol::Float64=1e-2)
 
     #parameters for the choice observation
-    pd = Dict("name" => vcat("bias","lapse"), "fit" => trues(2), 
+    pd = Dict("name" => vcat("bias","lapse"), "fit" => trues(2),
         "initial" => vcat(0.,0.5))
-    
+
     pz, pd = optimize_model(pz, pd, data; n=n, show_trace=show_trace, iterations=iterations)
-    
+
     return pz, pd
-    
+
 end
 
 """
@@ -109,12 +109,12 @@ end
 function optimize_model(pz::Dict{}, pd::Dict{}, data; n=53,
         x_tol::Float64=1e-4, f_tol::Float64=1e-9, g_tol::Float64=1e-2,
         iterations::Int=Int(2e3), show_trace::Bool=true)
-        
+
     haskey(pz,"state") ? nothing : pz["state"] = deepcopy(pz["initial"])
     haskey(pd,"state") ? nothing : pd["state"] = deepcopy(pd["initial"])
-    
+
     pz = check_pz!(pz)
-            
+
     fit_vec = combine_latent_and_observation(pz["fit"], pd["fit"])
     p_opt, p_const = split_combine_invmap(pz["state"], pd["state"], fit_vec, data["dt"], pz["lb"], pz["ub"])
 
@@ -125,29 +125,29 @@ function optimize_model(pz::Dict{}, pd::Dict{}, data; n=53,
     opt_output, state = opt_ll(p_opt, ll; g_tol=g_tol, x_tol=x_tol, f_tol=f_tol, iterations=iterations,
         show_trace=show_trace)
     p_opt = Optim.minimizer(opt_output)
-    
+
     pz["state"], pd["state"] = parameter_map_f(p_opt)
     pz["final"], pd["final"] = pz["state"], pd["state"]
-    
+
     return pz, pd, opt_output, state
-        
+
 end
 
 function ll_wrapper(p_opt::Vector{TT}, data::Dict, parameter_map_f::Function;
         n::Int=53) where {TT <: Any}
-          
+
     pz, pd = parameter_map_f(p_opt)
-    
+
     LL = compute_LL(pz, pd, data; n=n)
-    
+
     return -LL
-              
+
 end
 
 """
-    compute_LL(pz,bias,data;n::Int=53)
+    compute_LL(pz::Vector{T}, pd::Vector{T}, data; n::Int=53)
 
-    compute LL for choice observation model
+    compute LL for choice observation model. returns a scalar
 
 """
 compute_LL(pz::Vector{T}, pd::Vector{T}, data; n::Int=53) where {T <: Any} = sum(LL_all_trials(pz, pd, data, n=n))
