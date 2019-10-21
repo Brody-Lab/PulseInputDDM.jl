39ac8757edb77c512835be1a5fb053a64e00e757
briandepasquale <depasquale@princeton.edu>
2019-10-20 23:58:49 -0400

    Changed docs compat in Project to 0.23
    Former-commit-id: 5383b14990a16da0ac6174ab27e2fb9b6beedcdd [formerly 7fccf1383b0cfc7db9268ba31e493e85f88d0884]
    Former-commit-id: d2d38fbd26a54e135e9c2e0e1c701f56acacfbe0
    Former-commit-id: 4a704a931c132a9a0d40d5b3f7ea7743adc40e23 [formerly c4748fb5606513509f9bcfc1161695f98f914100]
    Former-commit-id: 062546f17dc17f916361acdbbfabdb19f97d96dd
    Former-commit-id: 106d404f78609eddcce14292c34cfd34f3d2c9c2
    Former-commit-id: b0f8bb79137bc2d23f61082e90ac6aa05a03aa51
    Former-commit-id: c538df69920bef2c0942eb635385892deb07421f

--- docs/make.jl
+++ docs/make.jl
@@ -3,8 +3,8 @@ push!(LOAD_PATH,"../src/")
 using Documenter, pulse_input_DDM
 DocMeta.setdocmeta!(pulse_input_DDM, :DocTestSetup, :(using pulse_input_DDM); recursive=true)
 
-makedocs(sitename="pulse input DDM", 
-        modules = [pulse_input_DDM], 
+makedocs(sitename="pulse input DDM",
+        modules = [pulse_input_DDM],
         doctest=true,
         authors = "Brian DePasquale",
         format = Documenter.HTML(assets = ["assets/favicon.ico"]),
@@ -22,7 +22,7 @@ makedocs(sitename="pulse input DDM",
         "Index" => "links.md",
         "Functions" => "functions.md"
          ])
- 
+
 deploydocs(deps   = Deps.pip("mkdocs", "python-markdown-math"),
            repo = "github.com/PrincetonUniversity/pulse_input_DDM.git",
            versions = ["stable" => "v^", "v#.#"]
