bc21e6ea90dbd9be2095d504bb2e6abd14a23c3a
Brian DePasquale <depasquale@princeton.edu>
2019-10-23 13:36:34 -0400

    readded unc
    Former-commit-id: f01e9405ff8349216b0181a56b8bb312fecf1cbb [formerly 20ea7d92586208af7a86a9078d88c03ea91cebc4]
    Former-commit-id: eb2f5b3d5a9a6de1ec421f6b35d073a75515acb4
    Former-commit-id: 85750e551733f4d50c332bf028b7b75d293fd395 [formerly e2932a681aaa3667690e45348a3c3fcd71c3f70d]
    Former-commit-id: ecad2d71c7c0fe37ce4c909f99c3df2a023df6d4
    Former-commit-id: 9d9673b4e22d04a3283ebd1af92315f99dc00f27
    Former-commit-id: d5254c099da54818845bb735f6e085610fd2e8f7
    Former-commit-id: d4a96ba2ea6503c5123be132a36e8a147873a995

--- src/choice_model/wrapper_functions.jl
+++ src/choice_model/wrapper_functions.jl
@@ -132,6 +132,73 @@ function optimize_model(pz::Dict{}, pd::Dict{}, data::Dict{}; n::Int=53,
 end
 
 
+function optimize_model_unc(pz::Dict{}, pd::Dict{}, data; n=53,
+        x_tol::Float64=1e-4, f_tol::Float64=1e-9, g_tol::Float64=1e-2,
+        iterations::Int=Int(2e3), show_trace::Bool=true)
+        
+    haskey(pz,"state") ? nothing : pz["state"] = deepcopy(pz["initial"])
+    haskey(pd,"state") ? nothing : pd["state"] = deepcopy(pd["initial"])
+    
+    pz = check_pz!(pz)
+            
+    fit_vec = combine_latent_and_observation(pz["fit"], pd["fit"])
+    p_opt, p_const = split_combine_invmap(pz["state"], pd["state"], fit_vec, data["dt"], pz["lb"], pz["ub"])
+
+    parameter_map_f(x) = map_split_combine(x, p_const, fit_vec, data["dt"], pz["lb"], pz["ub"])
+    ll(x) = ll_wrapper(x, data, parameter_map_f, n=n)
+    opt_output, state = opt_ll(p_opt, ll; g_tol=g_tol, x_tol=x_tol, f_tol=f_tol, iterations=iterations,
+        show_trace=show_trace)
+    p_opt = Optim.minimizer(opt_output)
+    
+    pz["state"], pd["state"] = parameter_map_f(p_opt)
+    pz["final"], pd["final"] = pz["state"], pd["state"]
+    
+    return pz, pd, opt_output
+        
+end
+
+
+
+
+function compute_Hessian_unc(pz, pd, data; n::Int=53) where {TT <: Any}
+
+    fit_vec = combine_latent_and_observation(pz["fit"], pd["fit"])
+    p_opt, p_const = split_combine_invmap(pz["final"], pd["final"], fit_vec, data["dt"], pz["lb"], pz["ub"])
+
+    parameter_map_f(x) = map_split_combine(x, p_const, fit_vec, data["dt"], pz["lb"], pz["ub"])
+    ll(x) = ll_wrapper(x, data, parameter_map_f, n=n)
+
+    return H = ForwardDiff.hessian(ll, p_opt);
+
+end
+
+function compute_H_CI_unc!(pz, pd, data; n::Int=53)
+
+    H = compute_Hessian_unc(pz, pd, data; n=n)
+
+    gooddims = 1:size(H,1)
+
+    evs = findall(eigvals(H[gooddims,gooddims]) .<= 0)
+    otherbad = vcat(map(i-> findall(abs.(eigvecs(H[gooddims,gooddims])[:,evs[i]]) .> 0.5), 1:length(evs))...)
+    gooddims = setdiff(gooddims,otherbad)
+
+    fit_vec = combine_latent_and_observation(pz["fit"], pd["fit"])
+    p_opt, p_const = split_combine_invmap(pz["final"], pd["final"], fit_vec, data["dt"], pz["lb"], pz["ub"])
+
+    parameter_map_f(x) = map_split_combine(x, p_const, fit_vec, data["dt"], pz["lb"], pz["ub"])
+
+    CI = fill!(Vector{Float64}(undef,size(H,1)),1e8);
+
+    CI[gooddims] = 2*sqrt.(diag(inv(H[gooddims,gooddims])))
+
+    pz["CI_plus"], pd["CI_plus"] = parameter_map_f(p_opt + CI)
+    pz["CI_minus"], pd["CI_minus"] = parameter_map_f(p_opt - CI)
+
+    return pz, pd, H
+
+end
+
+
 
 
 
