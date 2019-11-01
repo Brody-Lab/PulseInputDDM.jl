8eada2e7f39f4188fd8233ce5eb7d978c4ab145d
Brian DePasquale <depasquale@princeton.edu>
2019-10-31 23:56:09 -0400

    added docs
    Former-commit-id: d8f7fc5da781bbc577d9f2532181de9d260dc5f3 [formerly d50a25997aac50bdf98246921a2f41d9f0b136e5]
    Former-commit-id: 36225e1d38d9948fa7e07ee93e1b2ccc0a03d343
    Former-commit-id: d5a591d0b840928bc831da55b981767a8b4b6d3e [formerly 73dbec50f8f5e66a8075978ad1c6b85fa62db2ee]
    Former-commit-id: e701f8faa9e37d166f2600a80cdca10b8a2a5139
    Former-commit-id: 3ef725fd42b17f18ed52444c17f171253e903efc
    Former-commit-id: 30c403046fd42395e577598c13a8b7b7101edd73
    Former-commit-id: f2b3299233d3a01b37d2d6054464ea7b39c09da6

--- docs/make.jl
+++ docs/make.jl
@@ -3,26 +3,23 @@ push!(LOAD_PATH,"../src/")
 using Documenter, pulse_input_DDM
 DocMeta.setdocmeta!(pulse_input_DDM, :DocTestSetup, :(using pulse_input_DDM); recursive=true)
 
+# Make changes here if you add to documentation
+pages = Any[Any["Home" => "index.md"],
+        "Basics" => Any["man/getting_the_package.md",
+          "man/choice_observation_model.md",
+          "man/neural_observation_model.md"],
+        "Other helpful info" => Any["man/vpn_is_annoying.md"],
+        "Development" =>  Any["man/development.md"],
+        "Index" =>  Any["links.md"],
+        "Functions" =>  Any["functions.md"]]
+
 makedocs(sitename="pulse input DDM",
         modules = [pulse_input_DDM],
         doctest=true,
         authors = "Brian DePasquale",
         format = Documenter.HTML(assets = ["assets/favicon.ico"]),
-        pages = Any[
-        "Home" => "index.md",
-        "Basics" => Any[
-          "man/getting_the_package.md",
-          "man/choice_observation_model.md",
-          "man/neural_observation_model.md"
-         ],
-        "Other helpful info" => Any[
-           "man/vpn_is_annoying.md"
-        ],
-        "Development" => "man/development.md",
-        "Index" => "links.md",
-        "Functions" => "functions.md"
-         ])
+        pages = pages)
 
-deploydocs(deps   = Deps.pip("mkdocs", "python-markdown-math"),
+deploydocs(deps = Deps.pip("mkdocs", "python-markdown-math"),
            repo = "github.com/Brody-Lab/pulse_input_DDM.git",
            devbranch = "dev", devurl = "dev")
