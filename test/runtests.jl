4f4d6b33608f52cba2469ee0eb2e5f3d90f1ff15
Brian DePasquale <depasquale@princeton.edu>
2019-12-06 23:33:19 -0500

    fixed gradient test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,4 @@
 using Test, pulse_input_DDM, LinearAlgebra
 
 @test round(compute_LL(;ntrials=10, rng=1), digits=2) ≈ -0.86
-@test round(norm(compute_gradient(;ntrials=10, rng=1)), digits=2) ≈ 31.16
+@test round(norm(compute_gradient(;ntrials=10, rng=1)), digits=2) ≈ 239830.39
