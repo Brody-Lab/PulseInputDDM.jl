a40638188acc7c416677186b7847b5c83cc39983
Brian DePasquale <depasquale@princeton.edu>
2019-12-16 10:18:54 -0500

    updated tests based on default params

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,5 +3,5 @@ using Test, pulse_input_DDM, LinearAlgebra
 θ, data = synthetic_data(;ntrials=10,rng=1)
 model = choiceDDM(θ, data)
 
-@test round(loglikelihood(model), digits=2) ≈ -3.59
-@test round(norm(gradient(model)), digits=2) ≈ 3.94
+@test round(loglikelihood(model), digits=2) ≈ -3.76
+@test round(norm(gradient(model)), digits=2) ≈ 13.7
