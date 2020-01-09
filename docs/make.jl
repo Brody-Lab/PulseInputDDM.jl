7c769f4fc5baa18e569b5df6824d8176cf2c1b29
Brian DePasquale <depasquale@princeton.edu>
2020-01-09 12:57:19 -0500

    neural example

--- docs/make.jl
+++ docs/make.jl
@@ -6,17 +6,21 @@ DocMeta.setdocmeta!(pulse_input_DDM, :DocTestSetup, :(using pulse_input_DDM); re
 
 EXAMPLE = joinpath(@__DIR__, "..", "examples", "recover_choice_model.jl")
 OUTPUT = joinpath(@__DIR__, "src/generated")
+Literate.markdown(EXAMPLE, OUTPUT)
+
+EXAMPLE = joinpath(@__DIR__, "..", "examples", "recover_neural_model.jl")
+OUTPUT = joinpath(@__DIR__, "src/generated")
+Literate.markdown(EXAMPLE, OUTPUT)
 
 # Make changes here if you add to documentation
 pages = Any["Home" => Any["index.md"],
         "Basics" => Any["man/getting_the_package.md", "man/choice_observation_model.md",
-                "man/neural_observation_model.md", "generated/recover_choice_model.md"],
+                "man/neural_observation_model.md", "generated/recover_choice_model.md",
+                "generated/recover_neural_model.md"],
         "Other helpful info" => Any["man/vpn_is_annoying.md"],
         "Development" => Any["man/development.md"],
         "Index" =>  Any["links.md"],
         "Functions" =>  Any["functions.md"]]
-
-Literate.markdown(EXAMPLE, OUTPUT)
 
 makedocs(sitename="pulse input DDM",
         modules = [pulse_input_DDM],
--- examples/recover_neural_model.jl
+++ examples/recover_neural_model.jl
@@ -1,4 +1,4 @@
-# ### Fitting a choice model
+# # Fitting a choice model
 # Blah blah blah
 
 using pulse_input_DDM
@@ -6,37 +6,32 @@ using pulse_input_DDM
 # ### Geneerate some data
 # Blah blah blah
 
-f, ncells, ntrials, nparams, n = "Sigmoid", [2,3], [100,200], 4, 53
+f, ncells, ntrials, nparams = "Sigmoid", [1,2], [10,5], 4
 
 θ_syn = θneural(θz = θz(σ2_i = 0.5, B = 15., λ = -0.5, σ2_a = 10., σ2_s = 1.2,
     ϕ = 0.6, τ_ϕ =  0.02),
     θy=[[Sigmoid() for n in 1:N] for N in ncells], ncells=ncells,
     nparams=nparams, f=f)
 
-data = synthetic_data(θ_syn, ntrials)
+data = synthetic_data(θ_syn, ntrials);
 
 # ### Optimize stuff
 # Blah blah blah
 
+n = 53
+
 θy0 = vcat(vcat(initialize_θy.(data, f)...)...)
 
-options = neuraloptions(ncells=ncells,
+options0 = neuraloptions(ncells=ncells,
     fit=vcat(falses(dimz), trues(sum(ncells)*nparams)),
-    x0=vcat([0.1, 15., -0.1, 20., 0.5, 0.8, 0.008], θy0))
+    x0=vcat([0., 30., 0. + eps(), 0., 0., 1. - eps(), 0.008], θy0))
+model = optimize(data, options0; iterations=2, outer_iterations=1)
 
-model = optimize(data, options)
+options = neuraloptions(ncells=ncells, x0=pulse_input_DDM.flatten(model.θ))
+model = optimize(data, options, n; iterations=2, outer_iterations=1)
 
-x = pulse_input_DDM.flatten(model.θ)
-options = neuraloptions(ncells=ncells, x0=x)
-
-model = optimize(data, options, n)
-
-# ### Compute Hessian
+# ### Compute Hessian and the confidence interavls
 # Blah blah blah
 
-H = Hessian(model, n)
-
-# ### Get the CIs from the Hessian
-# Blah blah blah
-
-CI, HPSD = CIs(model, H)
+H = Hessian(model, n, chuck_size=4)
+CI, HPSD = CIs(model, H);
