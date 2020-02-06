ff6ed5f394b62816377d9db540869ab3351eadd4
Diksha Gupta <dikshag@princeton.edu>
2020-02-06 15:47:04 -0500

    randomizing initial conditions

--- src/choice_model/wrappers.jl
+++ src/choice_model/wrappers.jl
@@ -13,12 +13,12 @@ function default_parameters(;generative::Bool=false)
 
     pz = Dict("name" => ["σ_i","B", "λ", "σ_a","σ_s","ϕ","τ_ϕ","η","α_prior","β_prior"],
               "fit" => vcat(false, true, false, false, true, false, false, true, true, true),
-              "initial" => [eps(), 2.,-0.01, 2., 0.1, 0.1, 0.04, 0.99, 2., 2.],
+              "initial" => [eps(), 4. + rand()*(7. - 4.),-0.001, 1., 2. + rand()* (4. - 2.), 0.2, 0.15, rand(), rand()+1., rand()+1.],
               "lb" => [0., 1., -5., 0., 0., 0.01, 0.005, 0., 0., 0.],
               "ub" => [2., 15., 5., 100., 5, 1.2, 1., 1., 20., 20.])
 
     if generative
-        pz["generative"] = [eps(), 4.+rand()*(7. - 4.), rand()*2. - 1., rand()*2., rand()*2., rand()*1.5, rand(), rand(), rand()+1., rand()+1.]
+        pz["generative"] = [eps(), 4. +rand()*(7. - 4.), rand()*2. - 1., rand()*2., rand()*2., rand()*1.5, rand(), rand(), rand()+1., rand()+1.]
         pd["generative"] = [0.,0.0]
         pz["initial"][pz["fit"] .== 0] = pz["generative"][pz["fit"] .== 0]
     end
