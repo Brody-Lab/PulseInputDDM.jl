e55cbce6d6b194b76323138fcd1948dd17c9138e
tyler <jboydmeredith@gmail.com>
2020-02-20 13:53:35 -0500

    ADD function to sample choices given data, model
    input is a file with a rawdata struct and a set of parameters

--- src/choice_model/sample_model.jl
+++ src/choice_model/sample_model.jl
@@ -1,4 +1,26 @@
 """
+    synthetic_data(Θ, file; rng=1, centered=false)
+
+Returns click_times and choices for a given set of parameters Θ and a rawdata struct contained in input file
+"""
+function synthetic_data(file::String; θ::θchoice=θchoice(), rng::Int = 1, dt::Float64=1e-2, centered::Bool=false)
+    data = read(matopen(file),"rawdata")
+    T = vec(data["T"])
+    L = vec(map(x-> vec(collect(x)), data[collect(keys(data))[occursin.("left", collect(keys(data)))][1]]))
+    R = vec(map(x-> vec(collect(x)), data[collect(keys(data))[occursin.("right", collect(keys(data)))][1]]))
+    click_times = clicks.(L, R, T)
+    binned_clicks = bin_clicks.(click_times, centered=centered, dt=dt)
+    inputs = choiceinputs.(click_times, binned_clicks, dt, centered)
+    ntrials = length(inputs)
+    rng = sample(Random.seed!(rng), 1:ntrials, ntrials; replace=false)
+
+    choices = pmap((inputs, rng) -> rand(θ, inputs, rng), inputs, rng)
+
+    return click_times, choices
+
+end
+
+"""
     synthetic_data(; θ=θchoice(), ntrials=2000, rng=1)
 
 Returns default parameters and ntrials of synthetic data (clicks and choices) organized into a choicedata type.
