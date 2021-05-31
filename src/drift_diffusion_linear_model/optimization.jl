ec7775879bcf6d570c51a82a2f10307a3d5618a0
thomas.zhihao.luo <thomas.zhihao.luo@gmail.com>
2021-05-31 11:23:38 -0400

    what about 'latent_one_trial' is causing an NaN barrier term?

--- src/drift_diffusion_linear_model/optimization.jl
+++ src/drift_diffusion_linear_model/optimization.jl
@@ -123,14 +123,13 @@ cross::Bool, dt::Float64, n::Int) where {T1<:Real}
     # @unpack nT, nL, nR = clickcounts
     # @unpack L, R = clicktimes
 
-    P = P0(σ2_i, a₀, n, dx, xc, dt)
+    sum(P0(σ2_i, a₀, n, dx, xc, dt).*λ)
 
     # La, Ra = adapt_clicks(ϕ,τ_ϕ,L,R; cross=cross)
     # F = zeros(T1, n, n)
     # @inbounds for t = 1:nT
     #     P,F = latent_one_step!(P,F,λ,σ2_a,σ2_s,t,nL,nR,La,Ra,M,dx,xc,n,dt)
     # end
-    sum(P.*λ)
 end
 
 """
