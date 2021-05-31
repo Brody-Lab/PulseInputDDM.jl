3ef3868c545e481d39d093df9f375c1b482cec78
thomas.zhihao.luo <thomas.zhihao.luo@gmail.com>
2021-05-31 14:45:11 -0400

    fdf

--- src/drift_diffusion_linear_model/optimization.jl
+++ src/drift_diffusion_linear_model/optimization.jl
@@ -78,22 +78,11 @@ function loglikelihood(model::DDLM)
     @unpack σ2_i, B, λ, σ2_a = θz
     @unpack n, dt = options
 
-    TT = typeof(σ2_i)
-    xc,dx = bins(B,n)
+    P, M, xc, dx = initialize_latent_model(σ2_i, B, λ, σ2_a, n, dt)
+    P = P0(σ2_i, n, dx, xc, dt)
 
-    P = zeros(TT,n)
-    P[ceil(Int,n/2)] = one(TT)
-    # M = transition_M(σ2_i,zero(TT),zero(TT),dx,xc,n,dt)
-    # P = M * P
-
-    # P, M, xc, dx = initialize_latent_model(σ2_i, B, λ, σ2_a, n, dt)
-    # P = P0(σ2_i, n, dx, xc, dt)
-
-    M = transition_M(zero(TT), zero(TT),zero(TT),dx,xc,n,dt)
-    # M = transition_M(σ2_a*dt,λ,zero(TT),dx,xc,n,dt)
-
-    sum(sum(M))*B*λ*σ2_a
-    # sum(choice_likelihood!(bias, xc, P, data[1].trials[1].choice, n, dx)) * (1 - lapse) + lapse/2
+    # sum(sum(P))*B*λ*σ2_a
+    sum(choice_likelihood!(bias, xc, P, data[1].trials[1].choice, n, dx)) * (1 - lapse) + lapse/2
 
     # M,xc,dx = initialize_DDLM(σ2_i, B, λ, σ2_a, n, dt)
     # sum(map(trialset->loglikelihood(θ, trialset, options, M, xc, dx), data))
