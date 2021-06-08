7ddc3c88a9d04a8381ca39f13c7620ed189bb99a
thomas.zhihao.luo <thomas.zhihao.luo@gmail.com>
2021-06-08 08:38:45 -0400

    fdfd

--- src/drift_diffusion_linear_model/optimization.jl
+++ src/drift_diffusion_linear_model/optimization.jl
@@ -195,7 +195,7 @@ RETURN
 
 -P: A vector of vectors specifying the probability of the latent variable in each bin. Each element of the outer array corresponds to an individual trial
 """
-function forwardpass!(abar, F, latentspec::latentspecification, P, θ::θDDLM, trialset::trialsetdata)
+function forwardpass!(abar::Any, F::Any, latentspec::latentspecification, P, θ::θDDLM, trialset::trialsetdata)
     @unpack α, B, k = θ
     @unpack lagged, trials = trialset
 
@@ -229,7 +229,7 @@ MODIFICATION
 RETURN
 -P: A vector specifying the probability of the latent variable in each bin
 """
-function forwardpass!(abar::Vector{T1}, F::Matrix{T1}, a₀::T1, latentspec::latentspecification, P::Vector{T1}, θ::θDDLM, trial::trialdata) where {T1<:Real}
+function forwardpass!(abar::Vector{T1}, F::Matrix{T1}, a₀::Any, latentspec::latentspecification, P::Vector{T1}, θ::θDDLM, trial::trialdata) where {T1<:Real}
     @unpack clickindices, clicktimes, choice = trial
     @unpack σ2_i, λ, σ2_a, σ2_s, ϕ, τ_ϕ = θ
     @unpack nT, nL, nR = clickindices
