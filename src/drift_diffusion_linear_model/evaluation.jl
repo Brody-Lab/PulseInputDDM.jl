a4aa6b519e3694505119a865b813db8e1c2d82fb
thomas.zhihao.luo <thomas.zhihao.luo@gmail.com>
2021-06-10 18:22:28 -0400

    minor

--- src/drift_diffusion_linear_model/evaluation.jl
+++ src/drift_diffusion_linear_model/evaluation.jl
@@ -52,7 +52,7 @@ function predict_in_sample(model::DDLM)
     P = map((abar, trialset)->pulse_input_DDM.forwardpass!(abar, latentspec, θ, trialset), abar, data)
     choicelikelihood = map((P,trialset)->pmap((P, trial)->sum(choice_likelihood!(bias,xc,P,trial.choice,n,dx)), P, trialset.trials), P, data)
 
-    nprepad_abar = size(a_bases[1])[1]-1
+    nprepad_abar = size(a_bases[1])[1]
     Xa = map(abar->hcat(map(basis->vcat(pmap(abar->DSP.filt(basis, abar)[nprepad_abar+1:end], abar)...), a_bases)...), abar)
     β = map((trialset, Xa)->map(unit->leastsquares(unit, Xa, trialset.Xtiming), trialset.units), data, Xa)
     ŷ = map((β, trialset, Xa)->map(β->predict_spike_train(autoreg_bases, β, trialset.nbins_each_trial, Xa, trialset.Xtiming), β), β, data, Xa)
