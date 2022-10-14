ca78f75052e443df81727d33a708a49eebf0919a
Brian DePasquale <brian.depasquale@icloud.com>
2022-10-13 21:16:31 -0400

    make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -43,4 +43,4 @@ makedocs(sitename="pulse input DDM",
 deploydocs(deps = Deps.pip("mkdocs", "python-markdown-math"),
            repo = "github.com/Brody-Lab/pulse_input_DDM.git",
            branch = "gh-pages", devbranch = nothing,
-           devurl = "dev", versions = ["stable" => "v^", "v#.#.#", "dev" => "dev"])
+           devurl = "dev", versions = ["v#.#.#"])
