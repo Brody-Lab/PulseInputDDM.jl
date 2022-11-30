5780674ffefb435f020420d1e26a251eb5b241a9
Brian DePasquale <depasquale@princeton.edu>
2022-11-30 13:34:58 -0500

    fixed make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -40,13 +40,13 @@ makedocs(sitename="pulse input DDM",
 #           repo = "github.com/Brody-Lab/pulse_input_DDM.git",
 #           devbranch = "dev", devurl = "dev")
 
-deploydocs(deps = Deps.pip("mkdocs", "python-markdown-math"),
-           repo = "github.com/Brody-Lab/PulseInputDDM.git",
-           branch = "gh-pages", devbranch = nothing,
-           devurl = "dev", versions = ["stable" => "v^", "v#.#.#", "dev" => "dev"])
+#deploydocs(deps = Deps.pip("mkdocs", "python-markdown-math"),
+#           repo = "github.com/Brody-Lab/PulseInputDDM.git",
+#           branch = "gh-pages", devbranch = nothing,
+#           devurl = "dev", versions = ["stable" => "v^", "v#.#.#", "dev" => "dev"])
 
 #deploydocs(deps = Deps.pip("mkdocs", "python-markdown-math"),
 #           repo = "github.com/Brody-Lab/pulse_input_DDM.git")
 
-#deploydocs(repo = "Brody-Lab/pulse-input-DDM")
+deploydocs(repo = "Brody-Lab/PulseInputDDM.git")
 
