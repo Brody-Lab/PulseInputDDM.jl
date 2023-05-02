d438972ca4db92eaec04002e5f8d6bbd46a45823
dikshag <dikshag@princeton.edu>
2023-05-02 11:21:42 +0100

    renaming the repo tp pulse_input_DDM_trialhistory

--- src/pulse_input_DDM_trialhistory.jl
+++ src/pulse_input_DDM_trialhistory.jl
@@ -7,7 +7,7 @@ and/or neural data from pulse-based evidence accumlation tasks.
 
 #__precompile__(false)
 
-module pulse_input_DDM
+module pulse_input_DDM_trialhistory
 
 using StatsBase, Distributions, LineSearches, JLD2
 using ForwardDiff, Distributed, LinearAlgebra
