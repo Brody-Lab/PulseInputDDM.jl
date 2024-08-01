3d1b89ea4b4647ac0bfb4192110499de85960133
Brian DePasquale <bddepasq@bu.edu>
2024-08-01 15:00:54 -0400

    added joint model tests

--- test/joint_model_tests.jl
+++ test/joint_model_tests.jl
@@ -1,22 +1,48 @@
-n, cross, ntrials = 53, false, 2
+n, cross = 53, false
+ncells, ntrials = [1,2], [3,4]
+f = [repeat(["Sigmoid"], N) for N in ncells]
+
+θ = θneural(θz = θz(σ2_i = 0.5, B = 15., λ = -0.5, σ2_a = 10., σ2_s = 1.2,
+    ϕ = 0.6, τ_ϕ =  0.02),
+    θy=[[Sigmoid() for n in 1:N] for N in ncells], f=f);
+
+data, = synthetic_data(θ, ntrials, ncells);
+model_gen = neuralDDM(θ, data, n, cross, θprior(μ_B=40., σ_B=1e6));
+
+spikes = map(x-> sum.(x), getfield.(vcat(data...), :spikes))
+
+@test all(spikes .== [[5], [16], [4], [2, 2], [17, 15], [19, 16], [8, 13]])   
+
+x = PulseInputDDM.flatten(θ)
+θy0 = vcat(vcat(θy.(data, f)...)...)
+options0 = neural_options_noiseless(f)
+x0=vcat([0., 30., 0. + eps(), 0., 0., 1. - eps(), 0.008], θy0)
+θ0 = θneural_noiseless(x0, f)
+model0 = noiseless_neuralDDM(θ0, data)
+x0 = PulseInputDDM.flatten(θ0)
+@unpack f = θ0
+model, = optimize(model0, options0; iterations=2, outer_iterations=1)
+x0 = vcat([0.1, 15., -0.1, 20., 0.5, 0.8, 0.008], PulseInputDDM.flatten(model.θ)[dimz+1:end])
+options = neural_options(f)  
+model = neuralDDM(θneural(x0, f), data, n, cross, θprior(μ_B=40., σ_B=1e6))
+model, = optimize(model, options; iterations=2, outer_iterations=1)
 
 options = neural_choice_options(f)
 
 choice_neural_model = neural_choiceDDM(θneural_choice(vcat(x0[1:dimz], 0., 0., x0[dimz+1:end]), f), data, n, cross)
 
-@test round(choice_loglikelihood(choice_neural_model), digits=2) ≈ -6.45
+@test round(choice_loglikelihood(choice_neural_model), digits=2) ≈ -0.47
 
-@test round(joint_loglikelihood(choice_neural_model), digits=2) ≈ -486.23
+@test round(joint_loglikelihood(choice_neural_model), digits=2) ≈ -368.42
 
-import PulseInputDDM: nθparams
-nparams, = nθparams(f)
+nparams, = PulseInputDDM.nθparams(f)
 
 fit = vcat(falses(dimz), trues(2), falses.(nparams)...);
 options = neural_choice_options(fit=fit, lb=options.lb, ub=options.ub)
 
 choice_neural_model, = choice_optimize(choice_neural_model, options; iterations=2, outer_iterations=1)
 
-@test round(norm(PulseInputDDM.flatten(choice_neural_model.θ)), digits=2) ≈ 42.06
+@test round(norm(PulseInputDDM.flatten(choice_neural_model.θ)), digits=2) ≈ 56.29
 
 choice_neural_model = neural_choiceDDM(θneural_choice(vcat(x0[1:dimz], 0., 0., x0[dimz+1:end]), f), data, n, cross)
 
@@ -26,4 +52,4 @@ options = neural_choice_options(fit=fit, lb=vcat(options.lb[1:7], -10., options.
 
 choice_neural_model, = choice_optimize(choice_neural_model, options; iterations=2, outer_iterations=1)
 
-@test round(norm(PulseInputDDM.flatten(choice_neural_model.θ)), digits=2) ≈ 42.068
+@test round(norm(PulseInputDDM.flatten(choice_neural_model.θ)), digits=2) ≈ 56.29
